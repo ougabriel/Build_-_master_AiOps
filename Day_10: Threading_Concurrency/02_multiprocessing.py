b94e6649721112cc77031392d2984a4f6cb20e53
GABRIEL OKOM <34310658+ougabriel@users.noreply.github.com>
2025-12-28 23:03:37 +0000

    multiprocessing added to day10

---
+++ Day_10: Threading_Concurrency/02_multiprocessing.py
@@ -0,0 +1,25 @@
+## A simple program that brews a tea based on its name.
+
+from multiprocessing import Process
+import time
+
+def brew_tea(name):
+    print(f"Start {name} tea brewing.")
+    time.sleep(2)
+    print(f"End of {name} tea brewing")
+
+
+if __name__ == "__main__":
+    tea_maker = [
+        Process(target=brew_tea, args=(f"tea-maker #{i + 1}", ))
+        for i in range(3)
+    ]
+
+
+#start all process
+for p in tea_maker:
+    p.start()
+
+#wait for all to complete
+for p in tea_maker:
+    p.join()
