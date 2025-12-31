e5f87febf215ff1dd31a636edd288c4e4d687745
GABRIEL OKOM <34310658+ougabriel@users.noreply.github.com>
2025-12-31 00:24:45 +0000

    Multiprocessing examples using threads as a case study to show the difference in time, which is usually slower than using a processor

---
+++ Day_10: Threading_Concurrency/06_multiprocessing_queues_&_values/01_processor_one.py
@@ -0,0 +1,28 @@
+# A simple program that uses threads to perform a cpu intensive task; such as crunching a large number.
+
+import threading
+import time
+
+def cpu_heavy():
+    print("Crunching the numbers.")
+    total = 0
+    for i in range(10**9):
+        total += i
+    print("Done with the crunch")
+#create the thread
+threads = []
+for t in range(2):
+    t = threading.Thread(target=cpu_heavy, )
+    threads.append(t)
+
+start = time.time()
+for t in threads:
+    t.start()
+    
+for t in threads:
+    t.join()
+
+end = time.time()
+print(f"Finished in {end-start:.2f} secs.")
+
+
